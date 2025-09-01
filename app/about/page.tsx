d10ce6fedc9358f936323a67b7dd64b20cb61f60
Jummanbd <85425626+Jummanbd@users.noreply.github.com>
2025-09-02 04:32:28 +0600

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,14 +1,14 @@
-export async function getServerSideProps() {
-  // context এর মধ্যে থাকে query, params, req, res ইত্যাদি
-  const res = await fetch(
-    "https://explorerajasthantours.com/st_tour/delhi-goa-weekend-tour-package/"
-  );
-  const data = await res.json();
-  console.log(data);
+// export async function getServerSideProps() {
+//   // context এর মধ্যে থাকে query, params, req, res ইত্যাদি
+//   const res = await fetch(
+//     "https://explorerajasthantours.com/st_tour/delhi-goa-weekend-tour-package/"
+//   );
+//   const data = await res.json();
+//   console.log(data);
 
-  return {
-    props: {
-      data, // এই data component এ props হিসেবে যাবে
-    },
-  };
-}
+//   return {
+//     props: {
+//       data, // এই data component এ props হিসেবে যাবে
+//     },
+//   };
+// }
